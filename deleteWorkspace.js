e334c4f0ff2f09712e6f3cc76bf25e66b13fb366
Jim Madden <jmadden@twilio.com>
2019-01-30 07:51:27 -0800

    removed duplicate code

--- deleteWorkspace.js
+++ deleteWorkspace.js
@@ -3,7 +3,6 @@ require('dotenv-safe').config();
 const accountSid = process.env.ACCOUNT_SID;
 const authToken = process.env.AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
-const client = require('twilio')(accountSid, authToken);
 
 client.taskrouter
   .workspaces('################################')
